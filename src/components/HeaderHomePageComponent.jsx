1efd9035a6c53ed434c95065e25e1a311f6252f2
Milos1988 <vojvoda1988>
2023-07-03 23:49:07 +0200

    Navbar

--- src/components/HeaderHomePageComponent.jsx
+++ src/components/HeaderHomePageComponent.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import logo from "../assets/img/Logo-1.png"
+import React from "react";
+import logo from "../assets/img/Logo-1.png";
+import en from "../assets/img/england.png";
 
 const HeaderHomePageComponent = () => {
-    return (
-        <div className='pt-10'>
-             <div className='container mx-auto mt-10'>
-                <nav className='mx-48 flex ' >
-                    <img src={logo} alt="Logo" />
-                    <ul className='flex list-none'>
-                        <li>Početna</li>
-                        <li>O nama</li>
-                        <li>Usluge</li>
-                        <li>Klijenti</li>
-                        <li>Kontakt</li>
-                        <li>Kontakt</li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="pt-10">
+      <div className="container mx-auto mt-10">
+        <nav className="mx-48 flex justify-between ">
+          <img src={logo} alt="Logo" />
+          <div>
+            <ul className="flex list-none gap-x-3 uppercase">
+              <li>Početna</li>
+              <li>O nama</li>
+              <li>Usluge</li>
+              <li>Klijenti</li>
+              <li>Kontakt</li>
+              <li className="flex">
+                <img src={en} alt="" />
+                En
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
-export default HeaderHomePageComponent
+export default HeaderHomePageComponent;
