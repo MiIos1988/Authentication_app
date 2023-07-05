22dd01ea15894dba104993f027cc778675d3c1ff
Milos1988 <vojvoda1988@gmail.com>
2023-07-05 13:41:09 +0200

    Center logo

--- src/components/HeaderHomePageComponent.jsx
+++ src/components/HeaderHomePageComponent.jsx
@@ -6,10 +6,13 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 const HeaderHomePageComponent = () => {
     const [isMenuOpen, setMenuOpen] = useState(false);
     return (
-        <div className="pt-7 md:pt-10">
-            <div className="container mx-auto md:mt-10">
-                <nav className="mx-14 2xl:mx-48  md:flex justify-between items-center ">
-                    <img src={logo} alt="Logo" className="mx-auto md:mx-0" />
+        <div className="pt-0 md:pt-10">
+            <div className="container mx-auto">
+                <nav className="mx-14 2xl:mx-48  md:flex justify-between items-center my-6 md:my-10 h-14 ">
+                    <div className="flex h-full">
+
+                        <img src={logo} alt="Logo" className="mx-auto md:mx-0 my-auto self-center" />
+                    </div>
                     <RxHamburgerMenu className="flex  xl:hidden text-3xl  absolute right-3 top-10 md:static" />
                     <ul className="flex self-start list-none gap-x-3 uppercase hidden xl:flex">
                         <li>Početna</li>
