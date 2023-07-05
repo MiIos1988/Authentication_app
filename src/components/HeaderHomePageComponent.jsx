6cf6428d1b71e55d5b82ba6be1fa9ee4eae4a418
Milos1988 <vojvoda1988@gmail.com>
2023-07-05 09:26:01 +0200

    Add class apsolute for hamburger

--- src/components/HeaderHomePageComponent.jsx
+++ src/components/HeaderHomePageComponent.jsx
@@ -6,11 +6,11 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 const HeaderHomePageComponent = () => {
     const [isMenuOpen, setMenuOpen] = useState(false);
     return (
-        <div className="pt-10">
-            <div className="container mx-auto mt-10">
-                <nav className="mx-14 2xl:mx-48 flex justify-between items-center ">
-                    <img src={logo} alt="Logo" />
-                    <RxHamburgerMenu className="flex  xl:hidden text-3xl" />
+        <div className="pt-7 md:pt-10">
+            <div className="container mx-auto md:mt-10">
+                <nav className="mx-14 2xl:mx-48  md:flex justify-between items-center ">
+                    <img src={logo} alt="Logo" className="mx-auto md:mx-0" />
+                    <RxHamburgerMenu className="flex  xl:hidden text-3xl  absolute right-3 top-10 md:static" />
                     <ul className="flex self-start list-none gap-x-3 uppercase hidden xl:flex">
                         <li>Početna</li>
                         <li>O nama</li>
