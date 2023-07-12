efc35c564d06dcbd3e8ec5194c41eb66a9abd7a4
Milos1988 <vojvoda1988@gmail.com>
2023-07-12 13:50:42 +0200

    Config backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const portNumber = 5000;
+
+
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+app.listen(portNumber, (err) => {
+    err
+        ? console.log("Error on server start...")
+        : console.log(`Server is running on port ${portNumber}...`)
+}
+)
+
+
