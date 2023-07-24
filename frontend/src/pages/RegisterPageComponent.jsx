476175b48ca1fbbb7afecbc3e6eebd8f0a779cd6
Milos1988 <vojvoda1988@gmail.com>
2023-07-24 22:55:24 +0200

    Add object for validation register

--- frontend/src/pages/RegisterPageComponent.jsx
+++ frontend/src/pages/RegisterPageComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaUserAlt } from 'react-icons/fa';
 import { FaUnlock } from 'react-icons/fa';
 import { MdMail } from 'react-icons/md';
@@ -6,6 +6,20 @@ import { useTranslation } from 'react-i18next';
 
 const RegisterPageComponent = () => {
     const { t } = useTranslation();
+    const [registerInput, setRegisterInput] = useState({
+        firstName: "",
+        lastName: "",
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+
+    const fillRegisterInput = e => {
+        setRegisterInput({...registerInput, [e.target.name] : e.target.value})
+        
+    }
+
     return (
         <div className='container mx-auto'>
             <div className=' sm:mx-11 mx-2 2xl:mx-48 mt-8'>
@@ -13,31 +27,31 @@ const RegisterPageComponent = () => {
                 <form action="" className='flex flex-col w-[95%] sm:w-[420px] mx-auto align-center  shadow-[35px_35px_60px_15px_rgba(0,0,0,0.7)]  sm:p-10 p-5 rounded-xl' >
                     <h1 className='text-white text-3xl uppercase m-auto pb-8'>{t("register")}</h1>
                     <div className='relative'>
-                        <input type="text" placeholder={t("register.firstName")} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
+                        <input type="text" placeholder={t("register.firstName")} name='firstName' onChange={fillRegisterInput} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
                         < FaUserAlt className='absolute top-[10px] left-2 text-2xl text-blue-900' />
                     </div>
                     <div className='relative'>
-                        <input type="text" placeholder={t("register.lastName")} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
+                        <input type="text" placeholder={t("register.lastName")} name='lastName' onChange={fillRegisterInput} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
                         < FaUserAlt className='absolute top-[10px] left-2 text-2xl text-blue-900' />
                     </div>
                     <div className='relative'>
-                        <input type="text" placeholder={t("username")} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
+                        <input type="text" placeholder={t("username")} name='username' onChange={fillRegisterInput} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
                         < FaUserAlt className='absolute top-[10px] left-2 text-2xl text-blue-900' />
                     </div>
                     <div className='relative'>
-                        <input type="email" placeholder={t("register.email")} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
+                        <input type="email" placeholder={t("register.email")} name='email' onChange={fillRegisterInput} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
                         < MdMail className='absolute top-[10px] left-2 text-2xl text-blue-900' />
                     </div>
                     <div className='relative'>
-                        <input type="password" placeholder={t("register.password")} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
+                        <input type="password" placeholder={t("register.password")} name='password' onChange={fillRegisterInput} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
                         <FaUnlock className='absolute top-[10px] left-2 text-2xl text-blue-900' />
                     </div>
                     <div className='relative'>
-                        <input type="password" placeholder={t("register.confirmPassword")} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
+                        <input type="password" placeholder={t("register.confirmPassword")} name='confirmPassword' onChange={fillRegisterInput} className='mb-3 p-2 pl-14 focus:outline-none w-full text-xl' />
                         <FaUnlock className='absolute top-[10px] left-2 text-2xl text-blue-900' />
                     </div>
 
-                    <button className='text-white bg-blue-700 mt-5 text-xl p-2 uppercase'>{t("register")}</button>
+                    <button className='text-white bg-blue-700 mt-5 text-xl p-2 uppercase' onClick={() => console.log(registerInput)}>{t("register")}</button>
                 </form>
 
             </div>
