ae34298e2d079696271232710f2c01fcdfd1a044
Milos1988 <vojvoda1988@gmail.com>
2023-08-24 12:34:13 +0200

    List users

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const verifyUserLogin = require('../validation/verifyUserLogin');
 const verifyAdminLogin = require('../validation/verifyAdminLogin');
+const UserModel = require('../models/userModel');
 const userRoute = express.Router();
 
-userRoute.get("/get-all-user", verifyUserLogin, verifyAdminLogin, (req, res) => {
-    console.log("okeee")
-    res.send("okeee")
+userRoute.get("/get-all-user", verifyUserLogin, verifyAdminLogin, async (req, res) => {
+    const allUsers = await UserModel.find({});
+    const forSendAllUsers = allUsers.map(obj => {
+        return newObj = {
+            firstName: obj.firstName,
+            lastName: obj.lastName,
+            email: obj.email,
+            picture: obj.picture,
+            role: obj.role,
+            isActive: obj.isActive,
+        }
+    })
+    res.send({forSendAllUsers})
 })
 
 
--- frontend/src/components/AccessUserComponent.jsx
+++ frontend/src/components/AccessUserComponent.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from "react";
 import { getAllUser } from '../service/userService';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
 const AccessUserComponent = () => {
+  const [allUsers, setAllUsers] = useState([])
   useEffect(() => {
     const fetchData = async () => {
       try {
         const allUser = await getAllUser();
-        console.log(allUser);
+        setAllUsers([...allUser.data.forSendAllUsers]);
+
       } catch (err) {
         toast.error("Login error!");
       }
@@ -20,10 +22,18 @@ const AccessUserComponent = () => {
 
   return (
     <>
-      <div className="flex flex-col w-[95%] sm:w-[600px] mx-auto align-center text-white uppercase text-2xl shadow-[35px_35px_60px_15px_rgba(0,0,0,0.7)]  sm:p-10 p-5 rounded-xl">
-        <h1> All users:</h1>
+      <div className="flex flex-col w-[95%] sm:w-[600px] mx-auto align-center  shadow-[35px_35px_60px_15px_rgba(0,0,0,0.7)]  sm:p-10 p-5 rounded-xl">
+        <h1 className='text-white uppercase text-2xl'> All users:</h1>
+        {
+          allUsers.map((user, id) => {
+            return <div key={id} className='flex text-white'>
+              <div>{user.email}</div>
+              <div>active</div>
+            </div>
+          })
+        }
       </div>
-      <ToastContainer 
+      <ToastContainer
         position="top-right"
         autoClose={3000}
         hideProgressBar={false}
