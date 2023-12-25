fee0d2b4fe89db53d8f9d9790b9572b9588a7993
Milos1988 <vojvoda1988>
2023-12-25 12:54:44 +0100

    Convert googleButtonComponent.jsx to typescript

--- frontend/src/components/GoogleButtonComponent.tsx
+++ frontend/src/components/GoogleButtonComponent.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 
-const GoogleButtonComponent = ({ onClick }) => {
-  return (
+type Props = {
+  onClick: () => void
+}
+
+const GoogleButtonComponent = ({ onClick }: Props) => (
     <button className="text-blue-700 bg-white mt-3 text-xl p-2 uppercase relative font-bold" onClick={onClick}>
       <FcGoogle className="absolute text-4xl top-1 " />
       Google
     </button>
   );
-};
+;
 
 export default GoogleButtonComponent;
