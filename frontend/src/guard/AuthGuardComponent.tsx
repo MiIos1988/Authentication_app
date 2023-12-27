df5b56b6cac4dbbf9691baaa5039b5283cecac51
Milos1988 <vojvoda1988@gmail.com>
2023-12-27 12:37:16 +0100

    Modified AuthGuardComponent to typescript.

--- frontend/src/guard/AuthGuardComponent.tsx
+++ frontend/src/guard/AuthGuardComponent.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom';
 import { isUser } from '../service/authService';
 
-const AuthGuardComponent = ({children}) => {
+const AuthGuardComponent = ({children}: { children: ReactNode }) => {
   return (
     isUser() ? <Navigate to={"/"}/> : children
   )
