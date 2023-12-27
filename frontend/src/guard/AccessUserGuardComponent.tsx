5f29970c49618f5f619d91d21a009ad0ba94240f
Milos1988 <vojvoda1988@gmail.com>
2023-12-27 12:34:40 +0100

    Modified AccessUserGuardComponent to typescript.

--- frontend/src/guard/AccessUserGuardComponent.tsx
+++ frontend/src/guard/AccessUserGuardComponent.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom';
-
 import { isAdmin } from '../service/authService';
 
-const AccessUserGuardComponent = ({children}) => {
+const AccessUserGuardComponent = ({children}: { children: ReactNode }) => {
     return (
         isAdmin() ? children : <Navigate to={"/"}/>
     )
