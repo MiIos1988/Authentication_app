b97b649e6526b0b697b92093bd14bc4cb8762c9e
Milos1988 <vojvoda1988@gmail.com>
2024-01-03 13:54:53 +0100

    Changed AuthGuardComponent

--- frontend/src/guard/AuthGuardComponent.tsx
+++ frontend/src/guard/AuthGuardComponent.tsx
@@ -1,11 +1,23 @@
-import React, { ReactNode } from 'react'
+// import React, { ReactNode } from 'react'
+// import { Navigate } from 'react-router-dom';
+// import { isUser } from '../service/authService';
+
+// const AuthGuardComponent = ({children}: { children: ReactNode }) => {
+//   return (
+//     isUser() ? <Navigate to={"/"}/> : children
+//   )
+// }
+
+// export default AuthGuardComponent
+
+import React, { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 import { isUser } from '../service/authService';
 
-const AuthGuardComponent = ({children}: { children: ReactNode }) => {
+const AuthGuardComponent: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    isUser() ? <Navigate to={"/"}/> : children
-  )
+    isUser() ? <Navigate to={"/"} /> : <>{children}</>
+  );
 }
 
-export default AuthGuardComponent
+export default AuthGuardComponent;
