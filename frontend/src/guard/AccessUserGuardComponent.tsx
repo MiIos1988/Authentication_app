bff86fcd6fa80347acc733753de8b029cc8e42f8
Milos1988 <vojvoda1988@gmail.com>
2024-01-04 23:03:45 +0100

    Corrected code.

--- frontend/src/guard/AccessUserGuardComponent.tsx
+++ frontend/src/guard/AccessUserGuardComponent.tsx
@@ -1,11 +1,9 @@
-import React, { ReactNode } from 'react'
-import { Navigate } from 'react-router-dom';
-import { isAdmin } from '../service/authService';
+import React, { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
+import { isAdmin } from "../service/authService";
 
-const AccessUserGuardComponent = ({children}: { children: ReactNode }) => {
-    return (
-        isAdmin() ? <>{children}</> : <Navigate to={"/"}/>
-    )
-}
+const AccessUserGuardComponent = ({ children }: { children: ReactNode }) => {
+  return isAdmin() ? <>{children}</> : <Navigate to={"/"} />;
+};
 
-export default AccessUserGuardComponent
+export default AccessUserGuardComponent;
