7d2259d7ed25da01133034593535381a3a2aafb5
Milos1988 <vojvoda1988@gmail.com>
2024-04-05 00:22:20 +0200

    Corrected code

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { useTranslation } from "react-i18next";
 import { useNavigate } from 'react-router-dom';
 import { isAdmin } from '../service/authService';
 
 const HomePage = () => {
+    
     const userStore = useSelector((store: any) => store.userSlicer.user);
     const { t } = useTranslation();
     const navigate = useNavigate();
     let welcomeMessage;
+    const [isAdminUser, setIsAdminUser] = useState<boolean>(false)
+
+    useEffect(() => {
+        setIsAdminUser(isAdmin());
+    },[userStore]
+    )
 
     if (!userStore || userStore?.role === "guest") {
         welcomeMessage = `${t("welcome")} - ${t("guest")}`
@@ -22,7 +29,7 @@ const HomePage = () => {
     return (
         <div className='flex flex-col justify-center'>
             <h1 className='text-2xl text-center text-white'>{welcomeMessage}</h1>
-            {isAdmin() &&
+            {isAdminUser &&
                 <button className='text-white bg-blue-700 mt-5 text-xl p-2 transition-transform duration-100 active:scale-95 m-auto' 
                 onClick={() => navigate("/access-users")}>
                     Grant access to users
